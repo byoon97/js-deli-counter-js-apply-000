430db0ceb8391d946d7e7ef9fc3b4c4f99b5eab4
Brandon Yoon <byoon5397@gmail.com>
2020-02-15 23:39:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function currentLine(line) {
   var str = "The line is currently: ";
   for (var i = 0; i < line.length; i += 1) {
     var name = line[i];
-    var string = i+1 + ". " + name;
+    var string = i+1 + ". " + name + ", ";
     str += string;
   }
 
