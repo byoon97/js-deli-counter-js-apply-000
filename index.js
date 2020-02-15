0c020c1607e0b61389d8d189625271520b611d4b
Brandon Yoon <byoon5397@gmail.com>
2020-02-15 23:51:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function nowServing(deliLine) {
 }
 
 function currentLine(line) {
-  if (line === 0) {
+  if (line.length === 0) {
     return "The line is currently empty.";
   }
   var str = "The line is currently: ";
