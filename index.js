5a5ce368821ef4104c1ca41d54dbc7419daac2a2
Brandon Yoon <byoon5397@gmail.com>
2020-02-15 23:51:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,6 +20,6 @@ function currentLine(line) {
   for (var i = 0; i < line.length; i += 1) {
     var name = line[i];
     var string = i+1 + ". " + name + ", ";
-    return str += string;
+    str += string;
   }
 }
