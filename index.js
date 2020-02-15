8455571d5c2d64da8489566264b666887cc4eef9
Brandon Yoon <byoon5397@gmail.com>
2020-02-15 23:09:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function takeANumber(katzDeliLine, person) {
   katzDeliLine.push(person);
-  var string = "Welcome " + person + ". You are number " + katzDeliLine.length " in line.";
+  var string = "Welcome " + person + ". You are number " + katzDeliLine.length + " in line.";
   return string;
 }
